abd9525f0193d6d3df04cbcdd580e2dc47e5d8ee
Zaki ul Hassan <staqiulhassan@gmail.com>
2026-09-16 15:12:59 +0500

    implement binary search tree with insertion and traversal

---
+++ BST.js
@@ -0,0 +1,64 @@
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      //   return this;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          //Left
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          //Right
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+  lookup(value) {}
+}
+
+// Tree ko JSOn mein print karne ke liye helper function...
+function traverse(node) {
+  if (node === null) {
+    return null; // Safe check for null nodes
+  }
+  const tree = {
+    value: node.value,
+  };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
+}
+
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+console.log(JSON.stringify(traverse(tree.root)));
+console.log(tree);
